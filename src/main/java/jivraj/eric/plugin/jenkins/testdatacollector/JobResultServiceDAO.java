b60f920de9d4f0be0ddcbf743cfcab52c98ad3f8
Eric Jivraj <ericjivraj@gmail.com>
2020-04-06 19:18:26 +0100

    Refactoring

--- src/main/java/jivraj/eric/plugin/jenkins/testdatacollector/JobResultServiceDAO.java
+++ src/main/java/jivraj/eric/plugin/jenkins/testdatacollector/JobResultServiceDAO.java
@@ -4,6 +4,7 @@ import java.net.UnknownHostException;
 
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
+import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 
